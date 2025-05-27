c2668a7110ff3165422b8e271f95b0932d0fbad7
rashmigaonkar28 <147134822+rashmigaonkar28@users.noreply.github.com>
2025-05-27 22:53:34 +0530

    Update subtractClass.java

--- subtractClass.java
+++ subtractClass.java
@@ -1,1 +1,19 @@
+class Addition extends Thread {
+	int a, b;
+	String title;
+
+	Addition(String title, int a, int b) {
+		super(title);
+		this.title = title;
+		this.a = a;
+		this.b = b;
+		start();
+	}
+
+	public void run() {
+		System.out.println(this);
+		int c = a + b;
+		System.out.println("Addition:" + c);
+	}
+}
 

99e5d94f143438dd7b50eeaee78ad28aded1dddd
RohiniBhat <rohinibhat05@gmail.com>
2025-05-27 22:52:59 +0530

    Update divisionClass.java

--- divisionClass.java
+++ divisionClass.java
@@ -1,1 +1,19 @@
+class Division extends Thread {
+	int a, b;
+	String title;
+
+	Division(String title, int a, int b) {
+		super(title);
+		this.title = title;
+		this.a = a;
+		this.b = b;
+		start();
+	}
+
+	public void run() {
+		System.out.println(this);
+		int c = a / b;
+		System.out.println("Division:" + c);
+	}
+}
 

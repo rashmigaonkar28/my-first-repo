a48b20046117074884f5a883403146b87cef8ba0
RohiniBhat <rohinibhat05@gmail.com>
2025-05-27 22:53:26 +0530

    Update multiplyClass.java

--- multiplyClass.java
+++ multiplyClass.java
@@ -1,1 +1,19 @@
+class Multiplication extends Thread {
+	int a, b;
+	String title;
+
+	Multiplication(String title, int a, int b) {
+		super(title);
+		this.title = title;
+		this.a = a;
+		this.b = b;
+		start();
+	}
+
+	public void run() {
+		System.out.println(this);
+		int c = a * b;
+		System.out.println("Multiplication:" + c);
+	}
+}
 
